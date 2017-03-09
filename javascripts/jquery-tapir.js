667cfdfe3adcf86d1aaf2df4fc49eb9adf0f1dd4
zrshz <zrshz@zrshztekiMacBook-Pro.local>
2017-03-09 20:39:44 +0800

    Site updated at 2017-03-09 12:39:44 UTC

--- javascripts/jquery-tapir.js
+++ javascripts/jquery-tapir.js
@@ -7,34 +7,36 @@
       el = this;
 
       settings = {
-	           token: false,
-	           query_param: 'query'
-	         };
+                   token: false,
+                   query_param: 'query'
+                 };
 
       if (options) {
-	$.extend(settings, options);
+        $.extend(settings, options);
       }
 
       if (!settings.token || settings.query_param == '') {
-	return this;
+        return this;
       }
 
+      // 搜索超时
+	setTimeout(function(){
+		settings.complete();
+	},3000);
+
       $.getJSON(
-	'http://tapirgo.com/api/1/search.json?token=' + settings.token + '&query=' + paramValue(settings.query_param) + '&callback=?', function(data){
-	  if(settings['complete']) { settings.complete() }
-	  $.each(data, function(key, val) {
-	  document.getElementById('search_results').style.display="block";
-	  document.getElementById('search_results').style.height="100%";
-	  document.getElementById('search_results').style.overflow="hidden";
-	      var str1 = val.content;
-	      var str2 = str1.substr(1, 250);
-	      str2 = str2.substr(0, Math.min(str2.length, str2.lastIndexOf(" ")));
-	    el.append('<div id="content" class="inner"><article class="post"><h2 class="title"><a href="' + val.link + '">' + val.title + '</a></h2><div class="entry-content"><div class="date">Published on: ' + (val.published_on).substr(0,10) + '</div></div><br><p>' + str2 + '...</p><br><a href="' + val.link +'">Read on →</a></article></div>');
-	    if(key >= (data.length-1)){
-	        el.append('<div id="content" class="inner"><center><h2 style="border-bottom: 1px solid #ddd;">Result ends</h2></center><br><br></div>');
-	    }
-	  });
-	}
+        'http://tapirgo.com/api/1/search.json?token=' + settings.token + '&query=' + paramValue(settings.query_param) + '&callback=?', function(data){
+          if(settings['complete']) { settings.complete() }
+          $.each(data, function(key, val) {
+          document.getElementById('search_results').style.display="block";
+          document.getElementById('search_results').style.height="100%";
+          document.getElementById('search_results').style.overflow="hidden";
+              var str1 = val.content;
+              var str2 = str1.substr(1, 300);
+              str2 = str2.substr(0, Math.min(str2.length, str2.lastIndexOf(" ")));
+            el.append('<div><a href="' + val.link + '"><span style="font-size:1.3em">' + val.title + '</span></a><div style="font-size:0.8em">Published on : ' + (val.published_on).substr(0,10) + '</div><p style="font-size:0.8em; line-height:1em;">' + str2 + '...</p></div>');
+          });
+        }
       );
 
       return this;
